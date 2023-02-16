ec770b957b7442dcf5b8badc663938da5103f3dd
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-02-16 17:03:17 -0600

    Vista

---
+++ PROYECTO/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using PROYECTO.Entidades;
+using PROYECTO.Negocio;
+
+Operaciones operaciones = new Operaciones();
+clsOperaciones clsOperaciones = new clsOperaciones();
+
+Console.WriteLine("Ingrese una palabra");
+
+operaciones.palabra = Console.ReadLine();
+
+Console.WriteLine("Su palabra contiene "+clsOperaciones.Conteo(operaciones)+" caracteres");
