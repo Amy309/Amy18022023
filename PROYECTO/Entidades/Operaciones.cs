79f07a17af831cbc5d5f22d8ef983a4d6c540bc8
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-02-16 17:02:35 -0600

    Crea variable

---
+++ PROYECTO/Entidades/Operaciones.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROYECTO.Entidades
+{
+    public class Operaciones
+    {
+        public string? palabra{set;get;}
+
+    }
+}
